4b2726aba05e216d1bcbce5a73759e7c06153aba
A703\1dam <danielleludovicoo@gmail.com>
2024-10-04 16:18:12 +0200

    actualizo los eercicios del examen

--- ExamenUD1/src/main/java/org/example/Examen.java
+++ ExamenUD1/src/main/java/org/example/Examen.java
@@ -31,24 +31,31 @@ public class Examen {
 
     public void Ejercicio2(){
 
+    //El siguiente programa debe calcular el minimo de azulejos que necesita una pared.
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Introduce tu nombre");
-        String nombre = entrada.next();
-        System.out.println("La obra de " + nombre);
+        String nombre = entrada.next(); //variable que recoge el nombre introducido anteriormente
+        System.out.println("La obra de " + nombre); //imprime por pantalla la obr5a de y el nombre introducido
 
         System.out.println("Introduce el ancho de la pared:");
-        int ancho = entrada.nextInt();
+        int ancho = entrada.nextInt(); //variable que recoge el valor del ancho
+
         System.out.println("Introduce el largo de la pared:");
-        int largo = entrada.nextInt();
+        int largo = entrada.nextInt(); //variable que recoge el valor del largo
+
         System.out.println("Introduce ancho azulejo:");
-        int anchoazulejo = entrada.nextInt();
+        int anchoazulejo = entrada.nextInt(); //recoge el valor del alto del azulejo
+
         System.out.println("Introduce largo azulejo:");
-        int largoazulejo = entrada.nextInt();
+        int largoazulejo = entrada.nextInt(); //recoge el valor del largo del azulejo
 
+        if (anchoazulejo == largoazulejo);//codicion para evitar que los azulejos no sean cuadrados.
+        System.out.println("El azulejo no puede ser un cuadrado");
 
-
-
+        if (anchoazulejo >= ancho || largoazulejo >= largo){ // condicion para evitar que los azulejos no sean más grandes que la pared.
+            System.out.println("El azulejo no puede ser mas grande que la pared");
+        }
     }
 
 
--- ExamenUD1/src/main/java/org/example/Main.java
+++ ExamenUD1/src/main/java/org/example/Main.java
@@ -5,11 +5,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        Examen examen = new Examen();
-        examen.Ejercicio1();
+        //Examen examen = new Examen();
+        //examen.Ejercicio1();
 
-       //Examen exaamen = new Examen();
-       // examen.Ejercicio2();
+       Examen examen = new Examen();
+       examen.Ejercicio2();
 
 
         }
